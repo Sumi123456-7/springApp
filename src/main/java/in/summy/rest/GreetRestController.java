fc43df29083d1bd51e96e486b7589b7ab8ac52bb
summy <summyy.mits@gmail.com>
2023-08-26 17:31:56 +0530

    added in the repo

---
+++ src/main/java/in/summy/rest/GreetRestController.java
@@ -0,0 +1,25 @@
+package in.summy.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetRestController {
+	
+	@GetMapping("/greet")
+	public String greetMsg() {
+		return "Good Morning";
+	}
+
+	@GetMapping("/wish")
+	public String wishMsg() {
+		return "all the best";
+	}
+	
+	@GetMapping("/contact")
+	public String contact() {
+		return "+91-8406968023";
+	}
+
+
+}
---
+++ src/test/java/in/summy/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.summy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
